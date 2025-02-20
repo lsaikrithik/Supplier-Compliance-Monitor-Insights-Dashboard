ec1d39ce9a30cfd4684f2752c5659c3b61282347
Saikrithik L <118360580+lsaikrithik@users.noreply.github.com>
2025-02-21 04:39:15 +0530

    Delete backend/test.py

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect("dbname=supplier_compliance_db user=postgres password='your_password' host=localhost")
-    print("Connected successfully!")
-    conn.close()
-except Exception as e:
-    print("Connection failed:", e)
